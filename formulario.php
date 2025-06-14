b60bfaf6561315224dfa377a2bcdc70fa8e80c3f
Juan Lotero <jlotero@unal.edu.co>
2025-06-14 10:34:38 -0500

    test

---
+++ formulario.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <!--Titulo 1-->
+        Este es un formulario, Amén
+    </h1>
+    <p> <!--Etiqueta para parrafo-->
+        Este es el formulario de prueba para la clase del Sabado 14 de Junio de 2025
+    </p>
+    <br> <!--Salto de linea-->
+    <hr> <!--Linea de separacion-->
+    <h4> <!--Titulo 4-->
+        Empecemos
+    </h4> 
+    </label>  
+    <i><strong><label for="nombre">Nombre: </label></strong></i>
+    <input type="nombre">
+    <br><br>
+    <i><strong><label for="apellido">Apellido: </label></strong></i>
+    <input type="apellido">
+    <br><br>
+    <i><strong><label for="numero">Numero: </label></strong></i>
+    <input type="numero">
+    <br><br>
+    <i><strong><label for="direccion">Direccion: </label></strong></i>
+    <input type="direccion">
+    <hr>
+    <h6> <!--Titulo 6-->
+        Todos los derechos reservados
+    </h6>
+
+    <form action="" method="get"></form>
+</body>
+</html>
